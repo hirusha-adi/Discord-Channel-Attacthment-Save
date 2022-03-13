b0c49b773dbf1e34b23c59b4ac225f106ec34a01
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-03-13 21:34:12 +0530

    better console output

--- main.py
+++ main.py
@@ -24,39 +24,51 @@ async def on_message(message):
 
 @client.command()
 async def save(ctx, limit=None):
+
+    # Default to 9999 messages check
     if limit == None:
         limit = 9999
 
     print(
-        f"+ Starting to save {limit} messages in {ctx.channel.name} | {ctx.channel.id}")
+        f"[!] Starting to save {limit} messages in {ctx.channel.name} | {ctx.channel.id}")
 
     iter = 1
-    async for message in ctx.channel.history(limit=int(limit)):
-        all_attatchments = message.attachments
-        if all_attatchments:
-            iter_2 = 1
-            for attachment in all_attatchments:
-                try:
-                    if str(attachment.filename) in os.getcwd():
-                        extension = str(attachment.filename).split(".")[-1]
-                        save_f_name = f"{attachment.filename}.{''.join(random.choice(ascii_lowercase) for x in range(4))}.{extension}"
-                    else:
-                        save_f_name = str(attachment.filename)
+    try:
+        async for message in ctx.channel.history(limit=int(limit)):
+            all_attatchments = message.attachments
+            if all_attatchments:
+                iter_2 = 1
+                for attachment in all_attatchments:
+                    try:
+                        if str(attachment.filename) in os.listdir(os.getcwd()):
+                            extension = str(attachment.filename).split(".")[-1]
+                            save_f_name = f"{attachment.filename}.{''.join(random.choice(ascii_lowercase) for x in range(4))}.{extension}"
+                        else:
+                            save_f_name = str(attachment.filename)
 
-                    await attachment.save(save_f_name)
+                        await attachment.save(save_f_name)
 
-                    print(
-                        f"+ Saved attachment: {iter}-{iter_2} | {save_f_name}")
-                except Exception as e:
-                    print(
-                        f"! Failed to save attatchment {iter}-{iter_2}.\n! Error: {e}")
-                iter_2 += 1
-            iter_2 = 1
-        else:
-            with open(f"{iter}.txt", "w", encoding="utf-8") as ftext:
-                ftext.write(str(message.content))
-            print(f"+ Saved message: {iter}")
-        iter += 1
+                        print(
+                            f"[+] Saved attachment: {iter}-{iter_2} | {save_f_name}")
+                    except Exception as e:
+                        print(
+                            f"[-] Failed to save attatchment {iter}-{iter_2}.\n[-] Error: {e}")
+                    iter_2 += 1
+                iter_2 = 1
+
+            else:
+                with open(f"{iter}.txt", "w", encoding="utf-8") as ftext:
+                    ftext.write(str(message.content))
+                print(f"[+] Saved message: {iter} | {iter}.txt")
+
+            iter += 1
+
+        print(f"[+] Saved all attacthments and messages\n\tTo: {os.getcwd()}")
+        await ctx.send(f"[+] Saved all attacthments and messages to `{os.getcwd()}`")
+
+    except Exception as e:
+        print(f"[-] Failed to run command\n[-] Error: {e}")
+        await ctx.send(f"[+] Saved all attacthments and messages to ```{e}```")
 
 
 with open("token.txt", "r", encoding="utf-8") as tokenfile:
