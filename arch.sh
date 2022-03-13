bd3126ed630fb1eccbe07b10bdcab30c77cbec8a
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-03-13 21:13:10 +0530

    install for arch

---
+++ arch.sh
@@ -0,0 +1,12 @@
+echo "Installing Dependencies"
+sudo pacman -Syy install git python python-pip --noconfirm
+pip install discord -U
+echo "Installed git, python, python-pip with apt"
+echo "Installed discord.py with pip"
+git clone "https://github.com/hirusha-adi/Discord-Channel-Attacthment-Save.git"
+cd ./Discord-Channel-Attacthment-Save
+echo "--> [Ctrl]+[Shift]+V to save the bot token"
+echo "--> [Ctrl]+O to save the file"
+sleep 4s
+nano ./token.txt
+python main.py
